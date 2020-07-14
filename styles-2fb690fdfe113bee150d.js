947e8020172669fce149dc717c94210b95439c7e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-14 06:02:44 +0000

    Deploy JochenHiller/jochenontour to github.com/JochenHiller/jochenontour.git:gh-pages

--- styles-2fb690fdfe113bee150d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{248:function(n,o,w){},249:function(n,o,w){},390:function(n,o,w){}}]);
-//# sourceMappingURL=styles-2fb690fdfe113bee150d.js.map
--- sw.js
+++ sw.js
@@ -26,43 +26,27 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6cb18e2e5c185421f67d.js"
+    "url": "webpack-runtime-e9fa487654d48d92d063.js"
   },
   {
-    "url": "app-11af035f960449230677.js"
+    "url": "framework-5e713f507919262c2771.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-d8b4ccd21208c5bfa858.js"
+    "url": "app-7cc05d0bc88c77fb83af.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-017ff56a07047a3e40e7.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a5ccc7d8d3fbcad1779f9cbf091db18c"
+    "revision": "86f715a2f7b63285b9ed69b4d4ee3e64"
   },
   {
-    "url": "styles.251ce500e3c76186c197.css"
-  },
-  {
-    "url": "styles-2fb690fdfe113bee150d.js"
-  },
-  {
-    "url": "1-716e7afe81fda727b6d8.js"
-  },
-  {
-    "url": "2-610884cfbaa9e6630f7e.js"
-  },
-  {
-    "url": "3-eeb52584a1118d339684.js"
-  },
-  {
-    "url": "component---src-pages-404-jsx-400cba9ffa9b673c7220.js"
-  },
-  {
-    "url": "page-data/404.html/page-data.json",
-    "revision": "fe789893e8916f30f69925438c7d57c0"
+    "url": "polyfill-9c39ce6c05b9d7ca98f3.js"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "63f3762594e338e0bc43488fae474f4c"
+    "revision": "c568211c0af8ec6c9594352372f66a54"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
@@ -76,101 +60,46 @@ workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, workbox
 /* global importScripts, workbox, idbKeyval */
 
 importScripts(`idb-keyval-iife.min.js`)
-const WHITELIST_KEY = `custom-navigation-whitelist`
 
-const navigationRoute = new workbox.routing.NavigationRoute(({ event }) => {
-  const { pathname } = new URL(event.request.url)
+const { NavigationRoute } = workbox.routing
 
-  return idbKeyval.get(WHITELIST_KEY).then((customWhitelist = []) => {
-    // Respond with the offline shell if we match the custom whitelist
-    if (customWhitelist.includes(pathname)) {
-      const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
-      const cacheName = workbox.core.cacheNames.precache
+const navigationRoute = new NavigationRoute(async ({ event }) => {
+  let { pathname } = new URL(event.request.url)
+  pathname = pathname.replace(new RegExp(`^`), ``)
 
-      return caches.match(offlineShell, { cacheName }).then(cachedResponse => {
-        if (cachedResponse) return cachedResponse
+  // Check for resources + the app bundle
+  // The latter may not exist if the SW is updating to a new version
+  const resources = await idbKeyval.get(`resources:${pathname}`)
+  if (!resources || !(await caches.match(`/app-7cc05d0bc88c77fb83af.js`))) {
+    return await fetch(event.request)
+  }
 
-        console.error(
-          `The offline shell (${offlineShell}) was not found ` +
-            `while attempting to serve a response for ${pathname}`
-        )
-
-        return fetch(offlineShell).then(response => {
-          if (response.ok) {
-            return caches.open(cacheName).then(cache =>
-              // Clone is needed because put() consumes the response body.
-              cache.put(offlineShell, response.clone()).then(() => response)
-            )
-          } else {
-            return fetch(event.request)
-          }
-        })
-      })
+  for (const resource of resources) {
+    // As soon as we detect a failed resource, fetch the entire page from
+    // network - that way we won't risk being in an inconsistent state with
+    // some parts of the page failing.
+    if (!(await caches.match(resource))) {
+      return await fetch(event.request)
     }
+  }
 
-    return fetch(event.request)
-  })
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  return await caches.match(offlineShell)
 })
 
 workbox.routing.registerRoute(navigationRoute)
 
-let updatingWhitelist = null
-
-function rawWhitelistPathnames(pathnames) {
-  if (updatingWhitelist !== null) {
-    // Prevent the whitelist from being updated twice at the same time
-    return updatingWhitelist.then(() => rawWhitelistPathnames(pathnames))
-  }
-
-  updatingWhitelist = idbKeyval
-    .get(WHITELIST_KEY)
-    .then((customWhitelist = []) => {
-      pathnames.forEach(pathname => {
-        if (!customWhitelist.includes(pathname)) customWhitelist.push(pathname)
-      })
-
-      return idbKeyval.set(WHITELIST_KEY, customWhitelist)
-    })
-    .then(() => {
-      updatingWhitelist = null
-    })
-
-  return updatingWhitelist
-}
-
-function rawResetWhitelist() {
-  if (updatingWhitelist !== null) {
-    return updatingWhitelist.then(() => rawResetWhitelist())
-  }
-
-  updatingWhitelist = idbKeyval.set(WHITELIST_KEY, []).then(() => {
-    updatingWhitelist = null
-  })
-
-  return updatingWhitelist
-}
-
 const messageApi = {
-  whitelistPathnames(event) {
-    let { pathnames } = event.data
-
-    pathnames = pathnames.map(({ pathname, includesPrefix }) => {
-      if (!includesPrefix) {
-        return `${pathname}`
-      } else {
-        return pathname
-      }
-    })
-
-    event.waitUntil(rawWhitelistPathnames(pathnames))
+  setPathResources(event, { path, resources }) {
+    event.waitUntil(idbKeyval.set(`resources:${path}`, resources))
   },
 
-  resetWhitelist(event) {
-    event.waitUntil(rawResetWhitelist())
+  clearPathResources(event) {
+    event.waitUntil(idbKeyval.clear())
   },
 }
 
 self.addEventListener(`message`, event => {
   const { gatsbyApi } = event.data
-  if (gatsbyApi) messageApi[gatsbyApi](event)
+  if (gatsbyApi) messageApi[gatsbyApi](event, event.data)
 })
